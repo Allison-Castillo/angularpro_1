e78291e6f8d02b683bbd309e5e7b80ce1f9a36ab
Allison-Castillo <allison.castillobbc@gmail.com>
2026-08-28 09:50:54 -0400

    actu

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './components/home/home/home';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
